89e373b73385bd2bcb3c0db6ce34ea01eefa4ca0
Toby16 <bolajibaasit14@gmail.com>
2023-07-26 02:41:26 -0400

    Catch me if you can! | Report: correction made

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,12 @@
+
 export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+/*
 // function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     if (promise !== undefined) {
@@ -10,6 +18,7 @@ export default function handleResponseFromAPI(promise) {
     }
     console.log('Got a response from the API');
   });
+*/
 }
 
 /*
@@ -22,5 +31,5 @@ handleResponseFromAPI.then((data) => {
 
 /*
 const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+handleResponseFromAPI();
 */
