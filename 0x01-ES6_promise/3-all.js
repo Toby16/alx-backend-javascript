bc7a9dd36e10d275c29ee4d05e8a024966106694
Toby16 <bolajibaasit14@gmail.com>
2023-07-26 04:49:24 -0400

    Handle multiple successful promises | Report: fixes made | commiting to test with checker

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,26 @@
 import { uploadPhoto, createUser } from './utils';
 
+const uploadPhotoVar = uploadPhoto();
+const createUserVar = createUser();
+
 export default function handleProfileSignup() {
+  /*
   let body;
   let firstName;
   let lastName;
+  */
 
-  return uploadPhoto().then((data) => {
+  return Promise.all([uploadPhotoVar, createUserVar])
+    .then((data) => {
+      console.log(data[0].body, data[1].firstName, data[1].lastName);
+    }).catch(() => {
+      console.log('Signup system offline');
+    });
+
+  /*
+  return uploadPhotoVar.then((data) => {
     body = data.body;
-    return createUser();
+    return createUserVar;
   }).then((data) => {
     firstName = data.firstName;
     lastName = data.lastName;
@@ -15,4 +28,7 @@ export default function handleProfileSignup() {
   }).catch(() => {
     console.log('Signup system offline');
   });
+  */
 }
+
+// handleProfileSignup();
