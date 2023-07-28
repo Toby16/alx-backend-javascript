6a2544e244eaf21f5d218b781b6554db2fc84f81
Toby16 <bolajibaasit14@gmail.com>
2023-07-27 21:41:08 -0400

    Inheritance | Report: task done

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,27 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this._floors} floors`);
+  }
+}
+
+/*
+const building = new SkyHighBuilding(140, 60);
+console.log(building.sqft);
+console.log(building.floors);
+console.log(building.evacuationWarningMessage());
+*/
