88aee04a2c5714198a5c06b7ca6a893a3c35e8d5
Toby16 <bolajibaasit14@gmail.com>
2023-07-27 22:02:37 -0400

    Primitive - Holberton Class | Report: task done

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,21 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
+
+/*
+const hc = new HolbertonClass(12, "Mezzanine")
+console.log(hc)
+console.log(Number(hc));
+console.log(String(hc));
+*/
