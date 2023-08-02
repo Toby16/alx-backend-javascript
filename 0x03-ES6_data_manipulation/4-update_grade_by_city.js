13721f5c8389ce48a4aad4f76500de1c56b8ff97
Toby16 <bolajibaasit14@gmail.com>
2023-08-01 20:02:46 -0400

    Combine | Report: task Done

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,19 @@
+// import getListStudents from "./0-get_list_students.js";
+
+export default function updateStudentGradeByCity(getListStudentsVar, city, newGrades) {
+  const defaultGrade = { grade: 'N/A' };
+
+  if (getListStudentsVar instanceof Array) {
+    return getListStudentsVar
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
+}
