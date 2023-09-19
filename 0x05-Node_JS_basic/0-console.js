4f28013f98be2298399777f10cd8dea3d26f8bf5
Toby16 <bolajibaasit14@gmail.com>
2023-09-19 11:59:27 -0400

    Executing basic javascript with Node JS | Report: Task Done

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,16 @@
+/**
+ * Executing basic javascript with Node JS
+ *
+ * a function named 'displayMessage'
+ * that prints in STDOUT, the string argument.
+ *
+ * @param -
+ *  'stringArg' - The message to display (String).
+ */
+
+const displayMessage = (stringArg) => {
+  process.stdout.write(stringArg);
+};
+
+// displayMessage("Hello NodeJS!");
+module.exports = displayMessage;
