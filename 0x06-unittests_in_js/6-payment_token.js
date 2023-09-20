3ba75be04de5b844193c01353cd1f2d7802b0037
Toby16 <bolajibaasit14@gmail.com>
2023-09-20 19:01:29 -0400

    Async tests with done | Report: Task Done

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,11 @@
+/**
+ * Async tests with done
+ */
+
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
+  }
+});
+
+module.exports = getPaymentTokenFromAPI;
---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+/**
+ * Async tests with done
+ *
+ * Test cases for 'getPaymentTokenFromApi' function
+ */
+
+describe('getPaymentTokenFromAPI', () => {
+  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
+        done();
+      });
+  });
+});
