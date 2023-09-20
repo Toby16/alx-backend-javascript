e7e5703c63364ff2be2489d263af1e0072aeec12
Toby16 <bolajibaasit14@gmail.com>
2023-09-19 20:53:14 -0400

    Using Process stdin | Report: Task Done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,31 +2,16 @@
  * Using Process stdin
  */
 
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-function runProgram() {
-  const interfaceObject = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+process.stdin.on('readable', () => {
+  const INPUT = process.stdin.read();
 
-  interfaceObject.question('Welcome to Holberton School, what is your name?\n', (INPUT) => {
-    console.log(`Your name is: ${INPUT}`);
+  if (INPUT) {
+    process.stdout.write(`Your name is: ${INPUT}`);
+  }
+});
 
-    interfaceObject.close();
-
-    // Add this line to handle the 'close' event when the program is manually closed
-    process.stdin.on('end', () => {
-      console.log('This important software is now closing\n');
-    });
-  });
-}
-
-// Call the function to run the program
-runProgram();
-
-// Listen for Ctrl+C to gracefully exit the program
-process.on('SIGINT', () => {
-  console.log('This important software is now closing\n');
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
