a0b2b6a2ee1e66b53cd3abb293224bea96160263
Toby16 <bolajibaasit14@gmail.com>
2023-09-20 18:54:59 -0400

    Spies | Report: Task Done

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,12 @@
+const Utils = require('./utils');
+
+/**
+ * Spies
+ */
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
+};
+
+module.exports = sendPaymentRequestToApi;
