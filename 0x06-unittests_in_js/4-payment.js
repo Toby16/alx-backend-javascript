3b2bb627eb38f21e6b896a47dd1748cece7e3ff9
Toby16 <bolajibaasit14@gmail.com>
2023-09-20 18:57:33 -0400

    Stubs | Report: Task Done

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,12 @@
+const Utils = require('./utils');
+
+/**
+ * Stubs
+ */
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
+};
+
+module.exports = sendPaymentRequestToApi;
