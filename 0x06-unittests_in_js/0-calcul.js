d392d41bb9bb4cac58e87565ef89fe5c456cf48f
Toby16 <bolajibaasit14@gmail.com>
2023-09-20 18:36:15 -0400

    Basic test with Mocha and Node assertion library | Report: Task Done

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+/**
+ * Basic test with Mocha and Node assertion library
+ *
+ * calculateNumber - Function to return sum of two numbers
+ */
+
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+
+module.exports = calculateNumber;
